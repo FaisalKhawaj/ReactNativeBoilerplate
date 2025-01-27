b7609f2fce836bcf69585fe18b97b065e771785e
FaisalKhawaj <khawajfaisal981@gmail.com>
2025-01-28 01:15:42 +0500

    fixes to i18n

--- src/context/I18nProvider.tsx
+++ src/context/I18nProvider.tsx
@@ -5,18 +5,28 @@ import React, {
   useEffect,
   ReactNode,
 } from 'react';
-import {i18n, translate} from '../locales';
+import {useTranslation} from 'react-i18next'; // Import useTranslation
+
+import {translate, i18n} from '../locales';
 
 interface I18nContextProps {
   currentLocale: string;
   translate: typeof translate;
-  toggleLocale: () => void;
+  handleLocale: (locale: string) => void;
+  i18nInstance: typeof i18n;
+  refreshKey: number; // New: A key to force updates
+
+
 }
 
 const I18nContext = createContext<I18nContextProps>({
-  currentLocale: i18n.locale,
+  currentLocale: i18n.language, // Use i18n.language
   translate,
-  toggleLocale: () => {}, // No-op by default
+  handleLocale: () => {}, // No-op by default
+  i18nInstance: i18n,
+  refreshKey: 0, // Default value
+
+
 });
 
 export const I18nProvider: React.FC<{
@@ -27,17 +37,34 @@ export const I18nProvider: React.FC<{
   initialLocale = 'en', // Default locale
 }) => {
   const [currentLocale, setCurrentLocale] = useState(initialLocale);
+  const {i18n: i18nInstance} = useTranslation(); // Use useTranslation to get i18n instance
+  const [refreshKey, setRefreshKey] = useState(0); // New state for forcing re-render
 
   useEffect(() => {
-    i18n.locale = currentLocale; // Update the locale in `i18n`
-  }, [currentLocale]);
+    i18nInstance.changeLanguage(currentLocale); // Use changeLanguage
+  }, [currentLocale, i18nInstance]);
 
-  const toggleLocale = () => {
-    setCurrentLocale(prevLocale => (prevLocale === 'en' ? 'sp' : 'en')); // Toggle between English and Austrian
+  const handleLocale = (locale: string) => {
+    i18nInstance.changeLanguage(locale);
+    setCurrentLocale(locale);
+    setRefreshKey(prev => prev + 1); // Increment key to trigger updates
   };
 
+  useEffect(() => {
+    const onChange = () => {
+      setCurrentLocale(i18nInstance.language);
+      setRefreshKey(prev => prev + 1); // Trigger re-render on language change
+    };
+    i18nInstance.on('languageChanged', onChange);
+    return () => {
+      i18nInstance.off('languageChanged', onChange);
+    };
+  }, [i18nInstance]);
+
+
   return (
-    <I18nContext.Provider value={{currentLocale, translate, toggleLocale}}>
+    <I18nContext.Provider value={{currentLocale, translate, handleLocale,i18nInstance,
+      refreshKey}}>
       {children}
     </I18nContext.Provider>
   );
